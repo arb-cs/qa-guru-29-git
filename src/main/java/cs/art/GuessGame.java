8ddd55d1b6e62d22cb0cbb567596bf00ad5588c5
arb-cs <trust@no_one.com>
2024-07-03 01:06:02 +0300

    Deleted unnecessary arguments.

--- src/main/java/cs/art/GuessGame.java
+++ src/main/java/cs/art/GuessGame.java
@@ -8,7 +8,7 @@ public class GuessGame {
         System.out.println("Hello! This is a simple guess game.");
 
         // TODO
-        Random random = new Random(nextInt(6));
+        Random random = new Random();
 
     }
 }
