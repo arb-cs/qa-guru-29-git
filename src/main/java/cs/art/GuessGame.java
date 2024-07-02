5a8d1466253d767957554ef9ef114a6925076716
arb-cs <trust@no_one.com>
2024-07-03 01:01:44 +0300

    Deleted greeting (Don't ask why).

--- src/main/java/cs/art/GuessGame.java
+++ src/main/java/cs/art/GuessGame.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 public class GuessGame {
     public static void main(String[] args) {
-        System.out.println("Hello! This is a simple guess game.");
 
         // TODO
 
